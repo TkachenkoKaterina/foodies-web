d04177da9ed9072c01e762e8c18d495e877e6880
MarynaKindras <maryna.kindras@gmail.com>
2024-06-09 22:51:45 +0300

    Adding Testimonials

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,16 @@
 import Hero from '../../components/Hero';
 // import Categories from '../../components/Categories';
 // import Recipes from '../../components/Recipes';
-// import Testimonials from '../../components/Testimonials';
+import Testimonials from '../../components/Testimonials';
 
 const HomePage = () => {
   return (
     <>
-        <h2>HomePage</h2>
-        <Hero />
-        {/* <Categories /> */}
-        {/* <Recipes /> */}
-        {/* <Testimonials /> */}
+      <h2>HomePage</h2>
+      <Hero />
+      {/* <Categories /> */}
+      {/* <Recipes /> */}
+      {<Testimonials />}
     </>
   );
 };
