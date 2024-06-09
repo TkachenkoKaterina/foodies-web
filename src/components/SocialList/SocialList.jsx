2afd527e4d58191c930f5e41621a08a32a023800
Dima Tytenko <amgpetronass@gmail.com>
2024-06-09 19:35:44 +0300

    added footer

---
+++ src/components/SocialList/SocialList.jsx
@@ -0,0 +1,50 @@
+import styles from './SocialList.module.scss';
+import { links } from '../../constants/links';
+import icons from '../../images/icons.svg';
+
+const list = [
+  {
+    id: '1',
+    href: links.facebook,
+    icon: 'icon-facebook',
+  },
+  {
+    id: '2',
+    href: links.instagram,
+    icon: 'icon-instagram',
+  },
+  {
+    id: '3',
+    href: links.youtube,
+    icon: 'icon-youtube',
+  },
+];
+
+const SocialList = () => {
+  return (
+    <ul className={styles.list}>
+      {list.map(item => (
+        <Item key={item.id} href={item.href} icon={item.icon} />
+      ))}
+    </ul>
+  );
+};
+
+export default SocialList;
+
+const Item = ({ href, icon }) => {
+  return (
+    <li className={styles.item}>
+      <a
+        href={href}
+        className={styles.link}
+        target="_blank"
+        rel="noreferrer noopener"
+      >
+        <svg className={styles.icon}>
+          <use href={`${icons}#${icon}`} />
+        </svg>
+      </a>
+    </li>
+  );
+};
