edd7ac930c433294469285474ea62e58f2a7bf5b
vitalii-kyiv <v.s.chernous@gmail.com>
2024-06-11 18:57:30 +0300

    feature/adding recipe add page, working on recipe add component

---
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -0,0 +1,11 @@
+import AddRecipeForm from '../../components/AddRecipeForm';
+
+const AddRecipePage = () => {
+  return (
+    <>
+      <AddRecipeForm />
+    </>
+  );
+};
+
+export default AddRecipePage;
