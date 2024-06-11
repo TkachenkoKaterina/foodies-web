ed851deae249d5312bbe6653ad62d2e07a62430e
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2024-06-11 20:22:28 +0300

    static 4 from full list

--- src/components/PopularRecipes/PopularRecipes.jsx
+++ src/components/PopularRecipes/PopularRecipes.jsx
@@ -1,23 +1,22 @@
 import { RecipeCard } from '../../ui-kit';
 import recipes from '../ResipeList/recipes.json';
 const PopularRecipes = () => {
+  const mostPopular = recipes.slice(0, 4);
   return (
     <>
       <h1>Popular Recipes</h1>
-      <ul key="popular">
-        {recipes.map(item, index => {
-          do {
-            return (
-              <RecipeCard
-                key={item._id}
-                title={item.title}
-                description={item.description}
-                owner={item.owner}
-                img={item.img}
-                id={item._id}
-              />
-            );
-          } while (index < 4);
+      <ul>
+        {mostPopular.map((item, index) => {
+          return (
+            <RecipeCard
+              key={item._id + index}
+              title={item.title}
+              description={item.description}
+              owner={item.owner}
+              img={item.img}
+              id={item._id}
+            />
+          );
         })}
       </ul>
     </>
