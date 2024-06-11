5b43b189ff9d3c11ff17c49c524972d46da3bbca
Iryna <100072177+IrynaSabinina@users.noreply.github.com>
2024-06-11 20:02:00 +0300

    limited to 4

--- src/components/PopularRecipes/PopularRecipes.jsx
+++ src/components/PopularRecipes/PopularRecipes.jsx
@@ -5,17 +5,19 @@ const PopularRecipes = () => {
     <>
       <h1>Popular Recipes</h1>
       <ul key="popular">
-        {recipes.map(item => {
-          return (
-            <RecipeCard
-              key={item._id}
-              title={item.title}
-              description={item.description}
-              owner={item.owner}
-              img={item.img}
-              id={item._id}
-            />
-          );
+        {recipes.map(item, index => {
+          do {
+            return (
+              <RecipeCard
+                key={item._id}
+                title={item.title}
+                description={item.description}
+                owner={item.owner}
+                img={item.img}
+                id={item._id}
+              />
+            );
+          } while (index < 4);
         })}
       </ul>
     </>
