58afe8f6c1862e9e87d4d48f67446b94025cb8d0
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-06-14 11:51:08 +0300

    add showingl categories by click to button

--- src/components/CategoriesItem/CategoriesItem.jsx
+++ src/components/CategoriesItem/CategoriesItem.jsx
@@ -10,7 +10,7 @@ const CategoriesItem = ({ name, handlerCategoryChoose }) => {
       <img src={images[name]} alt={ name } className={css['category-img']} />
       <div className={css['category-info']}>
         <h3 className={css['category-title']}>{ name }</h3>
-        <button type="button" onClick={ handlerCategoryChoose } className={css['category-btn']}>
+        <button type='button' onClick={ handlerCategoryChoose } className={css['category-btn']}>
           <svg width='18' height='18' className={css['category-icon']}>
             <use href={`${icons}#icon-arrow-up-right`}></use>
           </svg>
