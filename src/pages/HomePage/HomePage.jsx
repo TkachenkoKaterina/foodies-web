26c3c15a955c04bf0854dfe9a786706c404896dd
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-06-15 12:29:28 +0300

    add notification with error from backend when fetch recipes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,17 +1,18 @@
+import { useState } from 'react';
+
 import Hero from '../../components/Hero';
 import Recipes from '../../components/Recipes';
 import Categories from '../../components/Categories';
-
 import Testimonials from '../../components/Testimonials';
-import styles from './HomePage.module.scss';
-import { useState } from 'react';
+
+// import styles from './HomePage.module.scss';
 
 const HomePage = () => {
   const [category, setCategory] = useState('');
   // const categoryBtn = document.querySelector();
   const handlerCategoryChoose = e => {
     e.preventDefault();
-    console.log(e.currentTarget.parentNode.nodeName);
+    // console.log(e.currentTarget.parentNode.nodeName);
     if (e.currentTarget.nodeName === 'BUTTON') {
       setCategory(e.currentTarget.parentNode.firstChild.textContent);
     }
