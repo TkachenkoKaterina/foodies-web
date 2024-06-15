5026326a20fe429df14042ed1d713ae9d3bd2404
Dima Tytenko <amgpetronass@gmail.com>
2024-06-15 12:25:42 +0300

    added pagination to user page

--- src/ui-kit/ListPagination/ListPagination.jsx
+++ src/ui-kit/ListPagination/ListPagination.jsx
@@ -1,53 +1,24 @@
-import React, { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
 import styles from './ListPagination.module.scss';
 
-// const itemsData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-
-// function PaginatedItems({ currentItems }) {
-//   return (
-//     <>
-//       {currentItems &&
-//         currentItems.map(item => (
-//           <div key={item}>
-//             <h3>Item #{item}</h3>
-//           </div>
-//         ))}
-//     </>
-//   );
-// }
-
-function ListPagination({ itemsPerPage = 9 }) {
-  const [itemOffset, setItemOffset] = useState(0);
-
-  const endOffset = itemOffset + itemsPerPage;
-  const currentItems = itemsData.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(itemsData.length / itemsPerPage);
-
-  const handlePageClick = event => {
-    const newOffset = (event.selected * itemsPerPage) % itemsData.length;
-    setItemOffset(newOffset);
-  };
+function ListPagination({ total, itemsPerPage, currentPage, onPageChange }) {
+  const pageCount = Math.ceil(total / itemsPerPage);
 
   return (
-    <>
-      <PaginatedItems currentItems={currentItems} />
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="  "
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={2}
-        marginPagesDisplayed={1}
-        pageCount={pageCount}
-        previousLabel="  "
-        renderOnZeroPageCount={null}
-        currentPage={styles.current_page}
-        containerClassName={styles.pagination}
-        pageClassName={styles.page}
-        activeClassName={styles.page_active}
-        disabledClassName={styles.page_disabled}
-      />
-    </>
+    <ReactPaginate
+      breakLabel="..."
+      nextLabel="  "
+      pageCount={pageCount}
+      initialPage={currentPage - 1}
+      onPageChange={onPageChange}
+      pageRangeDisplayed={2}
+      marginPagesDisplayed={1}
+      previousLabel="  "
+      renderOnZeroPageCount={null}
+      containerClassName={styles.pagination}
+      pageClassName={styles.page}
+      activeClassName={styles.page_active}
+    />
   );
 }
 
