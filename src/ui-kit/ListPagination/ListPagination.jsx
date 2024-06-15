b190ff65583115fab6d169e5686571160a610401
MarynaKindras <maryna.kindras@gmail.com>
2024-06-15 10:58:25 +0300

    Update style

--- src/ui-kit/ListPagination/ListPagination.jsx
+++ src/ui-kit/ListPagination/ListPagination.jsx
@@ -17,7 +17,7 @@ import styles from './ListPagination.module.scss';
 //   );
 // }
 
-function ListPagination({ itemsPerPage = 1 }) {
+function ListPagination({ itemsPerPage = 9 }) {
   const [itemOffset, setItemOffset] = useState(0);
 
   const endOffset = itemOffset + itemsPerPage;
